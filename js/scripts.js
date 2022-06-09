aaf610a86cae1857f24798e7199b6d6b7fb3a155
Mike Wilkes <mwwilkes@gmail.com>
2022-06-08 19:33:17 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+const number1 = parseInt(prompt("enter a number pleez"));
+const number2 = parseInt(prompt("enter another numberz"));
+
+window.alert(add(number1,number2,));
+
