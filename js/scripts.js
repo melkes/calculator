b292f727fd5314913cfecaf8855e527f2a9396bc
Mike Wilkes <mwwilkes@gmail.com>
2022-06-08 21:01:46 -0700

    makes js work as intended

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,34 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
-const number1 = parseInt(prompt("enter a number pleez"));
-const number2 = parseInt(prompt("enter another numberz"));
+function subtract(number1, number2) {
+  return number1 - number2;
+}
 
-window.alert(add(number1,number2,));
+function multiply(number1, number2) {
+  return number1 * number2;
+}
 
+function divide(number1, number2) {
+ return number1 / number2;
+}
+
+function display(number1, number2) {
+  let addDisp = number1 + " + " + number2 + " = " + add(number1, number2);
+  let subDisp = number1 + " - " + number2 + " = " + subtract(number1, number2);
+  let multDisp = number1 + " x " + number2 + " = " + multiply(number1, number2);
+  let divDisp = number1 + " ÷ " + number2 + " = " + divide(number1, number2);
+  return addDisp + " , " + subDisp + multDisp + divDisp;
+}
+
+const promptNum1 = parseInt(prompt("enter a number pleez"));
+const promptNum2 = parseInt(prompt("enter another number"));
+
+
+window.alert("here's your numbers: " + display(promptNum1, promptNum2));
+
+// window.alert("your numbers subtracted: " + subtract(number1, number2));
+
+// window.alert("your numbers multiplied: " + multiply(number1, number2));
+
+// window.alert("your numbers divided: " + divide(number1, number2));
