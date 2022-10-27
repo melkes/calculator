68fa460a458f51c992d55a5a01ea11185904a647
Mike Wilkes <mikewilkes@outlook.com>
2022-10-26 20:19:37 -0700

    added second window prompt

--- js/scripts.js
+++ js/scripts.js
@@ -15,8 +15,10 @@ function multiply(number1, number2) {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert("Your numbers add up to "+add(number1, number2)+".");
 
-window.alert("The first number minus the second number is " + subtract(number1, number2)+".");
-window.alert("The first number divided by the second number is " + divide(number1, number2));
-window.alert("Multiplied together they equal " + multiply(number1, number2) + ".");
+window.alert("Your numbers are " + number1 + " and " + number2 + ". " + "They add up to " + add(number1, number2) + ".\n\n" + "The first number minus the second number is " + subtract(number1, number2) + ".\n\n" + "The first number divided by the second number is " + divide(number1, number2) + ".\n\n" + "Multiplied together they equal " + multiply(number1, number2) + ".\n");
+
+window.alert(number1 + " + " + number2 + " = " + add(number1, number2) + ".\n\n" + number1 + " - " + number2 + " = " + subtract(number1, number2) + ".\n\n" 
+
++ number1 + " ÷ " + number2 + " = " + divide(number1, number2) + ".\n\n" 
++ number1 + " x " + number2 + " = " + multiply(number1, number2) + ".\n\n");
